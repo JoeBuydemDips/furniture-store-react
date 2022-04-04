e7d271156d325ee01caae49a736192f092217aae
JoeBuydemDips <ksarkodie111@icloud.com>
2022-04-04 16:33:31 -0400

    Set up router (version 5).

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+// Main entry point for this folder
+import Home from "./HomePage";
+import Error from "./ErrorPage";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+import About from "./AboutPage";
+import Cart from "./CartPage";
+import Checkout from "./CheckoutPage";
+import PrivateRoute from "./PrivateRoute";
+
+export {
+  Home,
+  Error,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Checkout,
+  PrivateRoute,
+};
