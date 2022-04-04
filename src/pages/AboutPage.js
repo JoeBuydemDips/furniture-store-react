99015669d94d82302354ca420a64f89c9b075ee7
JoeBuydemDips <ksarkodie111@icloud.com>
2022-04-04 18:17:45 -0400

    About, checkout page.

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,30 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="" />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline"></div>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum
+              veniam facere sit nemo velit debitis voluptatibus omnis modi
+              beatae hic commodi, facilis alias eaque id laudantium, quod
+              perferendis cumque eveniet.
+            </p>
+          </div>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +52,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
