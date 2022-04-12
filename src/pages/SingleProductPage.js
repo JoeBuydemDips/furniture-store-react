b35d6536684e29f2a2cca41fdec68d88fdd70c3e
JoeBuydemDips <ksarkodie111@icloud.com>
2022-04-12 19:44:21 -0400

    Add Single product images and reviews.

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -61,11 +61,11 @@ const SingleProductPage = () => {
         <Link to="/products" className="btn">
           back to products
         </Link>
-        <div className="products-center">
-          <ProductImages />
+        <div className="product-center">
+          <ProductImages images={images} />
           <section className="content">
             <h2>{name}</h2>
-            <Stars />
+            <Stars stars={stars} reviews={reviews} />
             <h5 className="price">{formatPrice(price)}</h5>
             <p className="desc"> {description}</p>
             <p className="info">
