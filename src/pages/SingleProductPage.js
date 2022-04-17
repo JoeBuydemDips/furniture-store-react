f6b463db9f88074a7187b5c8e5b42bc8c211d3be
JoeBuydemDips <ksarkodie111@icloud.com>
2022-04-17 19:47:28 -0400

    Single product page. colors.

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -54,6 +54,7 @@ const SingleProductPage = () => {
     images,
   } = product;
 
+  console.log(product);
   return (
     <Wrapper>
       <PageHero title={name} product />
@@ -81,7 +82,7 @@ const SingleProductPage = () => {
               {company}
             </p>
             <hr />
-            {stock > 0 && <AddToCart />}
+            {stock > 0 && <AddToCart product />}
           </section>
         </div>
       </div>
