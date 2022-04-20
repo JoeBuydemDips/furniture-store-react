0a0e24446cae84509c87c7e3ece6465409fa1cd8
JoeBuydemDips <ksarkodie111@icloud.com>
2022-04-20 17:21:50 -0400

    Filter context setup

--- src/index.js
+++ src/index.js
@@ -11,8 +11,10 @@ import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.render(
   <React.StrictMode>
     <ProductsProvider>
-      {" "}
-      <App />{" "}
+      <FilterProvider>
+        {" "}
+        <App />{" "}
+      </FilterProvider>
     </ProductsProvider>
   </React.StrictMode>,
   document.getElementById("root")
