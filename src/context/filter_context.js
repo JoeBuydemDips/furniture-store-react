2f81d28a1f69647390473c2b75bc3f16f479e2a5
JoeBuydemDips <ksarkodie111@icloud.com>
2022-04-20 18:08:37 -0400

    Sort component- UI setup

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -15,7 +15,7 @@ import { useProductsContext } from "./products_context";
 const initialState = {
   filtered_products: [],
   all_products: [],
-  grid_view: true,
+  grid_view: false,
 };
 
 const FilterContext = React.createContext();
